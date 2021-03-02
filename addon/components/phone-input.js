716afae7ba54379ebfd537fb2344b7868d93536f
Luke Booth <luke.booth@cph.org>
2021-03-02 08:35:24 -0600

    [skip] Stopped calling update on initialize (2m)

--- addon/components/phone-input.js
+++ addon/components/phone-input.js
@@ -230,8 +230,6 @@ export default Component.extend({
     if (this.initialCountry) {
       this._iti.setCountry(this.initialCountry);
     }
-
-    this.update(this.number, this._metaData(_iti));
   },
 
   _formatNumber() {
